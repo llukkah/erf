814f21bae49aec7077ebeacf909f19e7f2effc74
llukkah <llukkah.rey@gmail.com>
2020-12-04 16:13:47 -0500

    cleaned up code

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import './About.css'
+import React from "react";
+import "./About.css";
 
-
-
-
-export default function About () {
-    return (
-        <div>
-                  <header className="App-header">
-        <p className="motto">
-          Fuck Cash Bail
-        </p>
+export default function About() {
+  return (
+    <div>
+      <header className="App-header">
+        <p className="motto">Fuck Cash Bail</p>
         <div className="about-div">
           <hr></hr>
-        <p className="about">The mission of the Emergency Release Fund is to ensure that no trans person at risk in New York City jails remains in detention before trial. If ​cash bail is set for a trans person in New York City and no bars to release are in place, ​bail will be paid by the Emergency Release Fund.</p>
+          <p className="about">
+            The mission of the Emergency Release Fund is to ensure that no trans
+            person at risk in New York City jails remains in detention before
+            trial. If ​cash bail is set for a trans person in New York City and
+            no bars to release are in place, ​bail will be paid by the Emergency
+            Release Fund.
+          </p>
         </div>
       </header>
-        </div>
-
-    )
+    </div>
+  );
 }
