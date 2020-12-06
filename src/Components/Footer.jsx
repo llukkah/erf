587acdaf87bc82030a323a85e847600331921ee0
llukkah <llukkah.rey@gmail.com>
2020-12-06 18:17:45 -0500

    added instagram logo in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Footer.css";
+import Instagram from '../Graphics/instagram.svg'
 
 export default function Footer() {
   const today = new Date();
   return (
     <div>
       <div className="footer-div">
-        <p className="footer-links"></p>
+        <p className="footer-links">
+            <img src={Instagram}/>
+        </p>
         <p className="footer-date">
           {" "}
           &copy; EMERGENCY RELEASE FUND {today.getFullYear()}{" "}
