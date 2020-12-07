1e85016fe83fc447140af7f165d556836746ac6b
llukkah <llukkah.rey@gmail.com>
2020-12-06 20:14:20 -0500

    fixed centered footer links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Footer.css";
-import Instagram from '../Graphics/instagram.svg'
-import Facebook from '../Graphics/facebook.svg'
-import Twitter from '../Graphics/twitter.svg'
+import Instagram from "../Graphics/instagram.svg";
+import Facebook from "../Graphics/facebook.svg";
+import Twitter from "../Graphics/twitter.svg";
 
 export default function Footer() {
   const today = new Date();
@@ -10,16 +10,15 @@ export default function Footer() {
     <div>
       <div className="footer-div">
         <div className="footer-links">
-            <img src={Instagram}/>
-            <img src={Facebook}/>
-            <img src={Twitter}/>
-            <p className="footer-date">          {" "}
-          &copy; EMERGENCY RELEASE FUND {today.getFullYear()}{" "}</p>
+          <img src={Instagram} />
+          <span><img src={Facebook} /></span>
+          <img src={Twitter} />
         </div>
-        {/* <div className="footer-date">
-          {" "}
-          &copy; EMERGENCY RELEASE FUND {today.getFullYear()}{" "}
-        </div> */}
+        <div className="footer-date">
+          <p>
+            &copy; EMERGENCY RELEASE FUND {today.getFullYear()}{" "}
+          </p>
+        </div>
       </div>
     </div>
   );
