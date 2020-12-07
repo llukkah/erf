f4a58ba81549f8b7dca36300b896b9852f23b29d
llukkah <llukkah.rey@gmail.com>
2020-12-06 22:27:19 -0500

    cleaned up code

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Nav.css";
-// import Logo from "../Graphics/LogoWhite.png";
 import LogoGradient from "../Graphics/LogoGradient.png";
 
 export default function Nav() {
@@ -8,17 +7,18 @@ export default function Nav() {
     <div>
       <div className="nav">
         <div className="logo">
-          {/* <img src={Logo} className="logo-image"/> */}
-          <img src={LogoGradient} className="logo-image"/>
-          {/* <p className="erf-nav-logo">EMERGENCY RELEASE FUND</p> */}
+          <img src={LogoGradient} className="logo-image" />
           <div className="erf-nav-logo">
-          <p>EMERGENCY</p>
-          <p>RELEASE</p>
-          <p>FUND</p>
+            <p>EMERGENCY</p>
+            <p>RELEASE</p>
+            <p>FUND</p>
           </div>
         </div>
-        {/* <p className="donate-link">DONATE</p> */}
-        <p><a href="#" className="donate-link">DONATE</a></p>
+        <p>
+          <a href="#" className="donate-link">
+            DONATE
+          </a>
+        </p>
       </div>
     </div>
   );
