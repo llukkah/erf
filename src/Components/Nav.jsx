5492e8e6fc9db2a2889aacc9f8489eb2943a1269
llukkah <llukkah.rey@gmail.com>
2020-12-06 22:23:47 -0500

    added logo to nav bar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Nav.css";
-import Logo from "../Graphics/LogoWhite.png";
+// import Logo from "../Graphics/LogoWhite.png";
+import LogoGradient from "../Graphics/LogoGradient.png";
 
 export default function Nav() {
   return (
     <div>
       <div className="nav">
         <div className="logo">
-          <img src={Logo} className="logo-image"/>
+          {/* <img src={Logo} className="logo-image"/> */}
+          <img src={LogoGradient} className="logo-image"/>
           {/* <p className="erf-nav-logo">EMERGENCY RELEASE FUND</p> */}
           <div className="erf-nav-logo">
           <p>EMERGENCY</p>
