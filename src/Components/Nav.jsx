23d8ecc689db6e3f39075fe222de0b5582c44813
llukkah <llukkah.rey@gmail.com>
2020-12-06 21:09:35 -0500

    added css to nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import "./Nav.css";
+import Logo from "../Graphics/LogoWhite.png";
 
 export default function Nav() {
   return (
     <div>
-        <div className="nav">
-            <p className="erf-nav-logo">EMERGENCY RELEASE FUND</p>
-            <p>DONATE</p>
+      <div className="nav">
+        <div className="logo">
+          <img src={Logo} className="logo-image"/>
+          {/* <p className="erf-nav-logo">EMERGENCY RELEASE FUND</p> */}
+          <div className="erf-nav-logo">
+          <p>EMERGENCY</p>
+          <p>RELEASE</p>
+          <p>FUND</p>
+          </div>
         </div>
-
-
-
-
-
+        {/* <p className="donate-link">DONATE</p> */}
+        <p><a href="#" className="donate-link">DONATE</a></p>
+      </div>
     </div>
   );
 }
