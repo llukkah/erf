52a8ea3e0fc939cf6f2ff9700f20f3df3a402683
llukkah <llukkah.rey@gmail.com>
2020-12-08 13:30:38 -0500

    added css to donate page

--- src/Components/Donate.jsx
+++ src/Components/Donate.jsx
@@ -1,30 +1,37 @@
 import React from "react";
-import Iframe from 'react-iframe'
-import "./Donate.css"
+import Iframe from "react-iframe";
+import "./Donate.css";
 
 export default function Donate() {
   return (
-    <div>
-      <p className="donate-p">Donate</p>
-      <p className="donate-1">Thank you for your generosity. Your contribution will be used to post bail for people held in NYC jails, prioritizing BIPOC trans people, medically high-risk or otherwise marginalized populations, and anyone within the broader LGBTQ+ spectrum. Additionally, we assist with ICE detention immigration bonds for LGBTQ+ detainees. 
-
-A small portion of your contribution may be used for post-release support kits and costs associated with posting bail (transportation, PPE, etc.).
-
-To donate to Emergency Release Fund:
-
-Text bail to (202) 858-1233</p>
-<p className="donate-1"> To donate with credit card, PayPal, Venmo, ApplePay givebutter.com/erf </p>
-
-<Iframe url="https://givebutter.com/embed/c/erf"
+    <div className="donate-container">
+      <div className="donate-1">
+      <p>
+        Thank you for your generosity. Your contribution will be used to post
+        bail for people held in NYC jails, prioritizing BIPOC trans people,
+        medically high-risk or otherwise marginalized populations, and anyone
+        within the broader LGBTQ+ spectrum. Additionally, we assist with ICE
+        detention immigration bonds for LGBTQ+ detainees.
+      </p>
+      <p>
+        A small portion of your contribution may be used for post-release
+        support kits and costs associated with posting bail (transportation,
+        PPE, etc.).
+      </p>
+      <p>Text bail to (202) 858-1233</p>
+      </div>
+      {/* <div className="donate-container"> */}
+      <Iframe
+        url="https://givebutter.com/embed/c/erf"
         width="450px"
         height="615px"
-        id="myId"
         className="givebutter"
         display="initial"
-        position="relative"
+        // position="relative"
         scrolling="no"
-        allowpaymentrequest="true"/>
-{/*   style="max-width: 601px;" frameborder="0"  seamless ></iframe><script src="https://givebutter.com/js/widget.js"></script> */}
+        allowpaymentrequest="true"
+      />
+      {/* </div> */}
     </div>
   );
 }
